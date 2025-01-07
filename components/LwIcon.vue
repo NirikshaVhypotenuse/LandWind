cbab46f4241c71ea72dc1667562dccc243caa46f
Niriksha <niriksha@vhypotenuse.com>
2025-01-07 22:00:34 +0530

    completed the basic structure

--- components/LwIcon.vue
+++ components/LwIcon.vue
@@ -40,7 +40,7 @@ defineProps<{
     <svg v-else-if="name === 'tickmark'" :class="['flex-shrink-0 w-5 h-5']" :fill="fill" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
     </svg>
-    <svg v-else-if="name ==='star'" viewBox="0 0 16 16" width="16" height="16" class="octicon octicon-star" aria-hidden="true">
+    <svg v-else-if="name ==='star'" viewBox="0 0 16 16" width="16" height="16" :class="['octicon octicon-star']" aria-hidden="true">
         <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z">
         </path>
     </svg>
